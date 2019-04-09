e5c8ed032f07ca6781b09e242055f5a5b172acb1
tkielarbb <tomek.kielar00@gmail.com>
2019-04-09 19:15:43 +0200

    -working prototype -not working % button -not working =/- button -not working AC button

--- Calculator Test/ViewController.swift
+++ Calculator Test/ViewController.swift
@@ -12,38 +12,80 @@ class ViewController: UIViewController {
     @IBOutlet var buttons: [UIButton]!
     @IBOutlet var buttonses: [UIButton]!
     @IBOutlet weak var result: UILabel!
-    var resultNumber: Double = 0
-    func setbuttons() {
-        for every in buttons {
-            every.titleLabel?.text = "\(every.tag-1)"
-            every.backgroundColor = .darkGray
-            every.titleLabel?.textColor = .white
+    var defaultValue: Double = 0
+    let settingUps = setups()
+    var firstNumber:Double = 0
+    var secondNumber:Double = 0
+    var char:String = ""
+    var previousValue = ""
+    var output : Double = 0
+    
+  
+    @IBAction func numbers(_ sender: UIButton) {
+        previousValue = result.text!
+        let pressedValue = sender.titleLabel?.text
+       // result.text = NSString(format: "\(previ)%i", value!) as String
+        if previousValue == String(0.0) || previousValue == "Error"  || previousValue == char {
+            result.text = "\(pressedValue!)"
         }
-        for every in buttonses {
-            switch every.tag {
-            case 11...13:
-                every.backgroundColor = .lightGray
-                every.titleLabel?.textColor = .white
-            case 14...18:
-                every.backgroundColor = .orange
-                every.titleLabel?.textColor = .white
-            case 19:
-                every.backgroundColor = .darkGray
-            default:
-                print("error in setting functional buttons")
-            }
+        else {
+        result.text = "\(previousValue+pressedValue!)"
         }
     }
-    func setLabel() {
-        result.text = "\(resultNumber)"
+    @IBAction func functionKeys(_ sender: UIButton) {
+        previousValue = result.text!
+        if previousValue == "0.0" {
+            result.text = "Error2"
+        }
+        else {
+            result.text = "\(previousValue)"
+            switch sender.tag {
+            case 11:
+                result.text = "\(defaultValue)"
+            case 12:
+                print("change to minus")
+            case 13...17:
+                firstNumber = Double(result.text!) as! Double
+                char = sender.titleLabel!.text!
+                result.text = char
+            case 18:
+                secondNumber = Double(result.text!) as! Double
+                switch char {
+                case "%":
+                    print("XD")
+                case "/":
+                    output = firstNumber/secondNumber
+                    result.text = "\(output)"
+                case "X":
+                    output = firstNumber*secondNumber
+                    result.text = "\(output)"
+                case "-":
+                    output = firstNumber-secondNumber
+                    result.text = "\(output)"
+                case "+":
+                    output = firstNumber+secondNumber
+                    result.text = "\(output)"
+                default:
+                    result.text = "Error"
+                }
+            default:
+                print("xd")
+                
+            }
+        }
+        print(char)
+        print("liczba1: \(firstNumber)")
+        print("liczba2: \(secondNumber)")
+        print("wynik: \(output)")
+        
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         //setting background
         view.backgroundColor = .black
-        setLabel()
-        setbuttons()
+        settingUps.setbuttons(numbers: buttons, functions: buttonses)
+        settingUps.setLabel(result: result, resultNumber: defaultValue)
     }
 
    
--- Calculator Test/functions.swift
+++ Calculator Test/functions.swift
@@ -7,3 +7,35 @@
 //
 
 import UIKit
+
+class setups {
+    func setbuttons(numbers: [UIButton],functions: [UIButton]) {
+        for every in numbers {
+            every.titleLabel!.text = "\(every.tag-1)"
+            every.backgroundColor = .darkGray
+            every.titleLabel?.textColor = .white
+            every.layer.cornerRadius = 15
+        }
+        for every in functions {
+            switch every.tag {
+            case 11...13:
+                every.backgroundColor = .lightGray
+                every.titleLabel?.textColor = .white
+                every.layer.cornerRadius = 15
+            case 14...18:
+                every.backgroundColor = .orange
+                every.titleLabel?.textColor = .white
+                every.layer.cornerRadius = 15
+            case 19:
+                every.backgroundColor = .darkGray
+                every.layer.cornerRadius = 15
+            default:
+                print("error in setting functional buttons")
+            }
+        }
+    }
+    func setLabel(result:UILabel,resultNumber: Double) {
+        result.text = "\(resultNumber)"
+        result.textColor = .white
+    }
+}
